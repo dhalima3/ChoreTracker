e4e4a8fe76b67f2e04cc0ef768f7d4564225950a
Daryl Halima <dhalima@gatech.edu>
2014-12-24 19:58:20 -0500

    blah

--- app.js
+++ app.js
@@ -2,13 +2,60 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var csrf = require('lusca').csrf();
 
+var _ = require('lodash');
+var MongoStore = require('connect-mongo')(session);
+var flash = require('express-flash');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var connectAssets = require('connect-assets');
+
+/**
+ * Controllers (route handlers).
+ */
+// var homeController = require('./controllers/home');
+// var userController = require('./controllers/user');
+// var apiController = require('./controllers/api');
+// var contactController = require('./controllers/contact');
+
+/**
+ * Routes
+ */
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+/**
+ * API keys and Passport configuration.
+ */
+// var secrets = require('./config/secrets');
+// var passportConf = require('./config/passport');
+
+/**
+ * Create Express server
+ */
 var app = express();
+
+/**
+ * Connect to MongoDB
+ */
+mongoose.connect(secrets.db);
+mongoose.connection.on('error', function() {
+  console.error('MongoDB Connection Error. Please make sure that MongoDB is running.');
+}); 
+
+/**
+ * CSRF whitelist.
+ */
+var csrfExclude = ['/url1', '/url2'];
+
+/**
+ * Express configuration
+ */
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,20 +69,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/**
+ * CSRF protection
+ */
+app.use(function(req, res, next){
+    if (_.contains(csrfExclude, req.path)) return next();
+    csrf(req, res, next);
+});
+
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+/**
+ * catch 404 and forward to error handler
+ */
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+/**
+ * error handlers
+ * 
+ * development error handler
+ * will print stacktrace
+ */
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
@@ -46,8 +105,11 @@ if (app.get('env') === 'development') {
     });
 }
 
-// production error handler
-// no stacktraces leaked to user
+/**
+ * production error handler
+ *
+ * no stacktraces leaked to user
+ */ 
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
